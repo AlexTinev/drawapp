adef40609c3ede092413b2b51af4701226dd544d
AlexTinev <alexandertinev@gmail.com>
2016-08-24 11:07:17 +0300

    [Point.java] even Cooled Method added!

--- Point.java
+++ Point.java
@@ -4,7 +4,8 @@ public class Point{
     public void draw(){
     }
 
-    public void VeryCoolMethod(){
-}
+    public void VeryCoolMethod(){}
+
+public void MethodForChampsOnly(){}
     // Some code here as well
 }
